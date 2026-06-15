98219dd37a02220aee30e2830b7c5676174feb57
Srinath <srinathkmr0031@gmail.com>
2026-06-15 23:33:27 +0530

    Added one message to test the project working or not.

--- maven/MavenDemo/src/main/java/org/example/Main.java
+++ maven/MavenDemo/src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello Maven");
     }
 }
