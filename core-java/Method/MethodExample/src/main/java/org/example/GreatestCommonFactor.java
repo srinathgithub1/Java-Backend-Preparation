636beb1d397ed7bcd55ba6586238c179d483be04
Srinath <srinathkmr0031@gmail.com>
2026-06-21 03:50:32 +0530

    Approch-2: Find GCD of two number.

--- core-java/Method/MethodExample/src/main/java/org/example/GreatestCommonFactor.java
+++ core-java/Method/MethodExample/src/main/java/org/example/GreatestCommonFactor.java
@@ -3,12 +3,16 @@ package org.example;
 public class GreatestCommonFactor {
 
     public static int GCD(int a,int b){
-        if(a==b) return a;
+        if(b==0) return a;
         /**
          * Here suppose a=15 and b=25
-         * Step-1: a!=b then b=a-b or b=a%b => b=15 => calling again GCD(15,15)
+         * Step-1: b!=0 then b=a%b => b=10 => calling again GCD(25,15%25=15)
+         * Step-2: b!=0 then b=a%b => b=10 => calling again GCD(15,25%15=10)
+         * Step-3: b!=0 then b=a%b => b=0 => calling again GVD(10,15%10=5)
+         * Step-3: b!=0 then b=a%b => b=0 => calling again GVD(5,10%5=0)
+         * Step-4: b==0 return a.
          *
          */
-        else return GCD(b,a>b?a-b:b-a);
+        else return GCD(b,a%b);
     }
 }
