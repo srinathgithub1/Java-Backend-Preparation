399ebd495d8abdfa48a4c491eb9e5f73d52527b9
Srinath <srinathkmr0031@gmail.com>
2026-06-22 23:41:54 +0530

    case-2 ans 3: Method Overloading.

--- core-java/Method-Overloading/src/main/java/org/example/Main.java
+++ core-java/Method-Overloading/src/main/java/org/example/Main.java
@@ -6,9 +6,8 @@ public class Main {
 //        This will give the correct output. it will call case-1 method.
         System.out.println(Max.max(10,20));
 
-//        This will give error because in Max class there is not any method which is taking
-//        parameter max(float,int). It will give Compiler error.
-//        System.out.println(Max.max(10.5,20));
+//         This will call the case-2 method
+        System.out.println(Max.max(40.2f,20));
 
 
     }
--- core-java/Method-Overloading/src/main/java/org/example/Max.java
+++ core-java/Method-Overloading/src/main/java/org/example/Max.java
@@ -12,4 +12,28 @@ public class Max {
     public static int max(int a, int b){
         return a>b?a:b;
     }
+
+    /**
+     * Case-1:
+     * this is not allowed. this is already available even if we change the name of
+     * parameter it will not chang the method signature.
+     * @param x
+     * @param y
+     * @return
+     */
+    public static int max(int x, int y){
+        return x>y?x:y;
+    }
+
+
+    /**
+     * Case-2:
+     * this is allowed.
+     * @param a
+     * @param b
+     * @return
+     */
+    public static float max(float a, int b){
+        return (a>b) ? a:b;
+    }
 }
