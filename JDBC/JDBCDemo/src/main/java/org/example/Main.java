e2efcc84a26d964e65cd7905a45029eec83599d8
Srinath <srinathkmr0031@gmail.com>
2026-07-05 18:09:58 +0530

    JDBC next() pointer explanaion.

--- JDBC/JDBCDemo/src/main/java/org/example/Main.java
+++ JDBC/JDBCDemo/src/main/java/org/example/Main.java
@@ -6,6 +6,9 @@ public class Main {
         String url="jdbc:postgresql://localhost:5432/Demo";
         String username="postgres";
         String password="Srinath@123";
+//        PostgreSQL by default taking string in lowercase, so it is better to take the database name and column name in lower case.
+        String sql="select sname, marks from student where sid=1";
+
 //        this is Optional
         Class.forName("org.postgresql.Driver");
 
@@ -14,12 +17,19 @@ public class Main {
         System.out.println("Connected to database successfully");
 
         Statement st=cn.createStatement();
-        ResultSet re=st.executeQuery("select * from student");
-        while(re.next()){
-            System.out.println(re.getString("sName"));
-        }
+
+        ResultSet re=st.executeQuery(sql);
+//        re.next() check in table if it is having next row then it will return true else false
+//        initialy pointer next() pointing to the header so first move the next row then print.
+        System.out.println(re.next());
+        System.out.println(re.next());
+
+
+
+
         re.close();
         st.close();
         cn.close();
+        System.out.println("Connection closed successfully");
     }
 }
