b47e3a71f083564c40fc0376c406ed79c43178ef
Srinath <srinathkmr0031@gmail.com>
2026-07-08 23:57:14 +0530

    JDBC - Inserted data using PreparedStatement.

--- JDBC/JDBCDemo/src/main/java/org/example/Main.java
+++ JDBC/JDBCDemo/src/main/java/org/example/Main.java
@@ -7,24 +7,26 @@ public class Main {
         String username="postgres";
         String password="Srinath@123";
 //        PostgreSQL by default taking string in lowercase, so it is better to take the database name and column name in lower case.
-        String sql="delete from student where sid=7";
+        String sql="insert into student values(?,?,?)";
 
 //        this is Optional
         Class.forName("org.postgresql.Driver");
 
-        Connection cn=
+        Connection con=
                 DriverManager.getConnection(url,username,password);
         System.out.println("Connected to database successfully");
 
-        Statement st=cn.createStatement();
+        PreparedStatement st=con.prepareStatement(sql);
 
-//        boolean status=st.execute(sql);
-//        System.out.println(status);
-        int rows=st.executeUpdate(sql);
-        System.out.println(rows + " rows deleted successfully");
+        st.setInt(1,8);
+        st.setString(2,"Sonu");
+        st.setInt(3,68);
+
+        int rows=st.executeUpdate();
+        System.out.println(rows + " rows inserted");
 
         st.close();
-        cn.close();
+        con.close();
         System.out.println("Connection closed successfully");
     }
 }
