a7994a8fcc5a16bb65d49ee4f31fedf5073a7bc9
Srinath <srinathkmr0031@gmail.com>
2026-07-09 00:33:42 +0530

    JDBC - Deleted single data using PreparedStatement.

--- JDBC/JDBCDemo/src/main/java/org/example/Main.java
+++ JDBC/JDBCDemo/src/main/java/org/example/Main.java
@@ -7,8 +7,7 @@ public class Main {
         String username="postgres";
         String password="Srinath@123";
 //        PostgreSQL by default taking string in lowercase, so it is better to take the database name and column name in lower case.
-        String sql="select * from student where sid=?";
-        String sql2="select * from student";
+        String sql="delete from student where sid=?";
 
 //        this is Optional
         Class.forName("org.postgresql.Driver");
@@ -19,31 +18,11 @@ public class Main {
 
         PreparedStatement st=con.prepareStatement(sql);
 
-        st.setInt(1,1);
-
-        ResultSet rs=st.executeQuery();
-        System.out.println("=================Displaying Single Record.====================");
-        while(rs.next()){
-            System.out.println("Student ID: "+rs.getString("sid"));
-            System.out.println("Student Name: "+rs.getString("sname"));
-            System.out.println("Student Marks: "+rs.getString("marks"));
-        }
-
-        /**
-         * Displaying all record.
-         */
-        System.out.println("=================Displaying All Record.====================");
-        PreparedStatement st2=con.prepareStatement(sql2);
-        ResultSet rs1=st2.executeQuery();
-        while(rs1.next()){
-            System.out.println("Student ID: "+rs1.getString("sid"));
-            System.out.println("Student Name: "+rs1.getString("sname"));
-            System.out.println("Student Marks: "+rs1.getString("marks"));
-            System.out.println("====================================================");
-        }
+        st.setInt(1,10);
+        int rows=st.executeUpdate();
+        System.out.println(rows + " deleted successfully");
 
         st.close();
-        st2.close();
         con.close();
         System.out.println("Connection closed successfully");
     }
